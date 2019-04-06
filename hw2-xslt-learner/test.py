aa54754d89c37ebbc4b0812208640ab86908a98c
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-04-06 15:34:15 +0200

    Refactor test class

--- hw2-xslt-learner/test.py
+++ hw2-xslt-learner/test.py
@@ -1,18 +1,27 @@
 from utils import *
+import os
+import unittest
 
-//hello world!
+class FeatureTest(unittest.TestCase):
 
-p = Page("C:\\Users\\Marco\\AGI\\RomaTreAGIW\\pages\\0.html", "//h1")
+    def setUp(self):
+        filename = os.path.join(os.path.dirname(__file__), 'test-input\example_basidati.html')
+        self.page = Page(filename, "//h1")
+
+    def test_featuresGenerator(self):
+        self.page.featuresGenerator()
+
+        for feature in self.page.featureList:
+            print("(" + str(feature.ftype) + " - " + str(feature.l) + " - " + str(feature.value) + ")")
+
+    def test_featuresCombiner(self):
+        combs = self.page.featuresCombiner(k_subset=1)
+
+        for c in combs:
+            for i in c:
+                print("(" + str(i.ftype) + " - " + str(i.l) + " - " + str(i.value) + ")")
+            print("-----------------")
 
 
-p.featuresGenerator()
-
-for feature in p.featureList:
-    print("(" + str(feature.ftype) + " - " + str(feature.l) + " - " + str(feature.value) + ")")
-
-combs = p.featuresCombiner(k_subset=1)
-
-for c in combs:
-    for i in c:
-        print("(" + str(i.ftype) + " - " + str(i.l) + " - " + str(i.value) + ")")
-    print("-----------------")
+if __name__ == '__main__':
+    unittest.main()
