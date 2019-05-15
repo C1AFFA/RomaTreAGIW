d9d9aae22f2b377b2fa09d829691a653fd0a1b92
C1AFFA <michele.ciaffarafa@gmail.com>
2019-05-15 16:54:11 +0200

    loaded bike dataset with small fixes on pages

--- hw3-foxlink-classifier/product-img-extractor.py
+++ hw3-foxlink-classifier/product-img-extractor.py
@@ -5,7 +5,7 @@ import time
 
 driver = webdriver.Firefox()
 
-driver.get("https://www.amazon.it/Nilox-Bicicletta-Elettrica-Assistita-Autonomia/dp/B07CZV3B38/ref=sr_1_3?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=bicicletta&qid=1557158791&s=gateway&sr=8-3")
+driver.get("https://www.chainreactioncycles.com/it/it/fuji-cross-1-3-cyclocross-bike-2019/rp-prod179657")
 time.sleep(3)
 images = driver.find_elements_by_tag_name('img')
 
